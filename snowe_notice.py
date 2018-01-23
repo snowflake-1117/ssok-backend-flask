35a13903b86fb362975e1eb18ef5921f32a2fd22
Herin Chung <lync2846@gmail.com>
2018-01-24 07:33:03 +0900

    seperate to function

--- snowe_notice.py
+++ snowe_notice.py
@@ -2,6 +2,20 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import time
 
+def callBoardList():
+    board = browser.find_element_by_xpath('//*[@id="messageListBody"]')
+    noticeTds = board.find_elements_by_css_selector('td.title')
+    for td in noticeTds:
+        try:
+            a = td.find_element_by_css_selector('a')
+            href = a.get_attribute('href')
+            print("-", href)
+            span = a.find_element_by_css_selector('span')
+            print("-", span.text)
+        except NoSuchElementException:
+            print("-", td.text)
+    return;
+
 url = 'https://snowe.sookmyung.ac.kr/bbs5/boards/notice'
 browser = webdriver.PhantomJS()
 browser.implicitly_wait(3)
@@ -9,19 +23,7 @@ browser.get(url)
 
 # print(browser.find_element_by_tag_name('html').text)
 time.sleep(10)
-
-board = browser.find_element_by_xpath('//*[@id="messageListBody"]')
-noticeTds = board.find_elements_by_css_selector('td.title')
-for td in noticeTds:
-    try:
-        a = td.find_element_by_css_selector('a')
-        href = a.get_attribute('href')
-        print("-",href)
-        span = a.find_element_by_css_selector('span')
-        print("-",span.text)
-    except NoSuchElementException:
-        print("-", td.text)
-
+callBoardList()
 
 browser.quit()
 
