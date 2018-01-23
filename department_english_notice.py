2310abc256063f84f969d0e60173e3f2ef06d872
Herin Chung <lync2846@gmail.com>
2018-01-23 15:40:41 +0900

    extracted href from list but not applied to js yet

---
+++ department_english_notice.py
@@ -0,0 +1,25 @@
+import urllib.request
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+url = "http://english.sookmyung.ac.kr/wiz/contents/board/board.php?home_id=english&handle=3"
+browser = webdriver.PhantomJS()
+browser.implicitly_wait(3)
+browser.get(url)
+
+titles = browser.find_elements_by_css_selector('td.list_td1')
+
+# get the lists
+#for title in titles:
+#        print ("-",title.text)
+
+for title in titles:
+    try:
+        a = title.find_element_by_css_selector("a")
+        print ("-",a.get_attribute("href"))
+        print(title.text)
+    except NoSuchElementException:
+        print(title.text)
+
+browser.quit()
+
