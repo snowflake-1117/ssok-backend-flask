10ecda1d5d0b0f68c42c19aa0a670203848e72ed
Herin Chung <lync2846@gmail.com>
2018-01-24 08:54:21 +0900

    execute_script jf_view because specific href is not descipted.

--- department_english_notice.py
+++ department_english_notice.py
@@ -1,15 +1,22 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 
+def jf_view():
+    browser.execute_script("return "+href)
+    return;
+
 def printList():
     titles = browser.find_elements_by_xpath('/html/body/form[2]/table/tbody/tr[*]/td[3]')
 
     for title in titles:
         try:
             a = title.find_element_by_css_selector("a")
+            global href
             href = a.get_attribute("href")
+            href = href[11:]
             print("-", href)
             print(title.text)
+            jf_view()
         except NoSuchElementException:
             print(title.text)
     return;
@@ -18,6 +25,8 @@ url = 'http://english.sookmyung.ac.kr/wiz/contents/board/board.php?home_id=engli
 browser = webdriver.PhantomJS()
 browser.implicitly_wait(3)
 browser.get(url)
+
+printList()
 
 browser.quit()
 
