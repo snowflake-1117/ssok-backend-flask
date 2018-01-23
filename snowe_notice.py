d1be1c7d33489544004c8565df62a41e01923cc5
Herin Chung <lync2846@gmail.com>
2018-01-24 07:29:00 +0900

    need ome delay for browser to process dynamic

--- snowe_notice.py
+++ snowe_notice.py
@@ -1,24 +1,26 @@
-import urllib.request
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+import time
 
 url = 'https://snowe.sookmyung.ac.kr/bbs5/boards/notice'
 browser = webdriver.PhantomJS()
 browser.implicitly_wait(3)
 browser.get(url)
 
-#print(browser.find_element_by_tag_name('html').text)
+# print(browser.find_element_by_tag_name('html').text)
+time.sleep(10)
+
 board = browser.find_element_by_xpath('//*[@id="messageListBody"]')
 noticeTds = board.find_elements_by_css_selector('td.title')
 for td in noticeTds:
-    a = td.find_element_by_css_selector('a')
-    href = a.get_attribute('href')
     try:
+        a = td.find_element_by_css_selector('a')
+        href = a.get_attribute('href')
         print("-",href)
         span = a.find_element_by_css_selector('span')
-        print(span.text)
+        print("-",span.text)
     except NoSuchElementException:
-        print("-", href)
+        print("-", td.text)
 
 
 browser.quit()
