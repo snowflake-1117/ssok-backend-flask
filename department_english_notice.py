55a8e827fa0d11f0dc3c32b0b7df15eaeb9188d1
Herin Chung <lync2846@gmail.com>
2018-01-25 16:40:37 +0900

    get correct list range

--- department_english_notice.py
+++ department_english_notice.py
@@ -7,15 +7,15 @@ def print_list():
     titles = browser.find_elements_by_css_selector('td.list_td1')
     list_len = len(titles)//5
     print('count list: '+str(list_len))
-    START_OF_LIST_TD = 4
+    START_OF_LIST_TD = 5
 
-    for count in range(1, list_len):
+    for count in range(0, list_len):
         try:
             a = browser.find_element_by_xpath('/html/body/form[2]/table/tbody/tr['+str(count+START_OF_LIST_TD)+']/td[3]/a')
             href = a.get_attribute("href")
             print("href: ", href)
             print("제목: ",a.text)
-            print_link(a)
+            #print_link(a)
         except NoSuchElementException:
             print("-",a.text)
 
