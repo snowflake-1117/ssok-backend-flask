0774a749fe30ca14f7fae59e574f5527310844a9
Herin Chung <lync2846@gmail.com>
2018-01-25 15:00:52 +0900

    catch submitted page

--- department_english_notice.py
+++ department_english_notice.py
@@ -11,11 +11,19 @@ def print_list():
             href = a.get_attribute("href")
             print("-", href)
             print("-",title.text)
-            # a.click()
+            a.click()
         except NoSuchElementException:
             print("-",title.text)
 
     return;
+
+def test_click():
+    a = browser.find_element_by_css_selector('body > form:nth-child(2) > table > tbody > tr:nth-child(7) > td:nth-child(3) > a')
+    href = a.get_attribute("href")
+    print("-", href)
+    a.click()
+    print("page_source",browser.page_source)
+    return
 
 url = 'http://english.sookmyung.ac.kr/wiz/contents/board/board.php?home_id=english&handle=3'
 browser = webdriver.PhantomJS()
@@ -23,7 +31,7 @@ browser.implicitly_wait(3)
 browser.get(url)
 time.sleep(5)
 #print(browser.find_element_by_css_selector('html').text)
-print_list()
-
+#print_list()
+test_click()
 browser.quit()
 
