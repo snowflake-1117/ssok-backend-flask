51aac94b6a02d81d33f0c128a619766b35c8cc95
Herin Chung <lync2846@gmail.com>
2018-01-25 23:34:01 +0900

    got the entire html but not the list

--- department_english_notice.py
+++ department_english_notice.py
@@ -6,21 +6,22 @@ def print_list():
     titles = browser.find_elements_by_css_selector('td.list_td1')
     list_len = len(titles)//5
     print('count list: '+str(list_len))
-    #START_OF_LIST_TD = 5
-    #current_url = browser.current_url
-    # for count in range(0, 2):
-    #     try:
-    #         a = browser.find_element_by_xpath('/html/body/form[2]/table/tbody/tr['+str(count+START_OF_LIST_TD)+']/td[3]/a')
-    #         #href = a.get_attribute("href")
-    #         #print("href: ", href)
-    #         print(str(count+1)+"."+"제목: ",a.text)
-    #         print_link(a,current_url)
-    #     except NoSuchElementException:
-    #         print("-",a.text)
-    move_to_next_page()
+    START_OF_LIST_TD = 5
+    count=4
+    if count==4:
+        try:
+            a = browser.find_element_by_xpath('/html/body/form[2]/table/tbody/tr['+str(count+START_OF_LIST_TD)+']/td[3]/a')
+            #href = a.get_attribute("href")
+            #print("href: ", href)
+            print(str(count+1)+"."+"제목: ",a.text)
+            print_link(a)
+        except NoSuchElementException:
+            print("-",a.text)
+    print('out of for')
     return
 
 def move_to_next_page():
+    print(browser.find_element_by_css_selector('html').text)
     tr_list = browser.find_elements_by_xpath('/html/body/form[2]/table/tbody/tr[*]')
     page_location = str(len(tr_list)-2)
     page_list = browser.find_elements_by_xpath('/html/body/form[2]/table/tbody/tr['+page_location+']/td/table/tbody/tr/td[2]/*')
@@ -29,20 +30,28 @@ def move_to_next_page():
     page_last = page_list.__getitem__(len(page_list)-1)
     LAST_NUMBER = page_last.text
     print('page:',CURRENT_NUMBER+'/'+LAST_NUMBER)
-    if(LAST_NUMBER !='[다음]' and CURRENT_NUMBER==LAST_NUMBER):
-        print("END OF PAGE")
-    else:
-        current_item_count = page_list.index(current_page)
-        a = page_list.__getitem__(current_item_count+1)
-        a.click()
-        time.sleep(5)
-        print_list()
+    current_item_count = page_list.index(current_page)
+    #print('current_item_count:',current_item_count)
+    next_page = page_list.__getitem__(current_item_count+1)
+    #print('next_page.text:',next_page.text)
+    next_page.click()
+    time.sleep(5)
+    #print_list()
+    move_to_next_page()
+    # if(LAST_NUMBER !='[다음]' and CURRENT_NUMBER==LAST_NUMBER):
+    #     print("END OF PAGE")
+    # else:
+    #     current_item_count = page_list.index(current_page)
+    #     a = page_list.__getitem__(current_item_count+1)
+    #     a.click()
+    #     time.sleep(5)
+    #     print_list()
     return
 
 
-def print_link(a,start_url):
-    # a.click()
-    # time.sleep(5)
+def print_link(a):
+    a.click()
+    time.sleep(5)
     # content_div =browser.find_element_by_xpath('//*[@id="contentsDiv"]')
     # print('<content>\n',content_div.text)
     # span_list = browser.find_elements_by_css_selector('span')
@@ -52,16 +61,15 @@ def print_link(a,start_url):
     #         content = content + span.text + '\n'
     # print('<content>\n',content)
     print('>')
-    browser.get(start_url)
+    browser.get(url)
     time.sleep(5)
     return
 
+url = 'http://english.sookmyung.ac.kr/wiz/contents/board/board.php?home_id=english&handle=3'
 browser = webdriver.PhantomJS()
 browser.implicitly_wait(3)
-browser.get('http://english.sookmyung.ac.kr/wiz/contents/board/board.php?home_id=english&handle=3')
+browser.get(url)
 time.sleep(5)
-
-print_list()
-
+move_to_next_page()
 browser.quit()
 
