b6306af08616c8e8f7965fb15bbe827923de611a
Herin Chung <lync2846@gmail.com>
2018-01-25 15:41:16 +0900

    extract only <a>

--- department_english_notice.py
+++ department_english_notice.py
@@ -3,26 +3,19 @@ from selenium.common.exceptions import NoSuchElementException
 import time
 
 def print_list():
-    titles = browser.find_elements_by_css_selector('td.list_td1')
-
-    for title in titles:
+    a_list = browser.find_elements_by_xpath('/html/body/form[2]/table/tbody/tr[*]/td[3]/a')
+    for a in a_list:
         try:
-            a = title.find_element_by_css_selector("a")
             href = a.get_attribute("href")
             print("-", href)
-            print("-",title.text)
-            test_click()
-            browser.get(board_url)
-            time.sleep(5)
+            print("-",a.text)
+            # print_link(a)
         except NoSuchElementException:
-            print("-",title.text)
+            print("-",a.text)
 
-    return;
+    return
 
-def test_click():
-    a = browser.find_element_by_css_selector('body > form:nth-child(2) > table > tbody > tr:nth-child(7) > td:nth-child(3) > a')
-    href = a.get_attribute("href")
-    print("-", href)
+def print_link(a):
     a.click()
     time.sleep(5)
     span_list = browser.find_elements_by_css_selector('span')
@@ -31,14 +24,16 @@ def test_click():
         if span.text:
             content = content + span.text + '\n'
     print('<content>\n',content)
+    browser.get(board_url)
+    time.sleep(5)
     return
 
+global board_url
 board_url = 'http://english.sookmyung.ac.kr/wiz/contents/board/board.php?home_id=english&handle=3'
 browser = webdriver.PhantomJS()
 browser.implicitly_wait(3)
 browser.get(board_url)
 time.sleep(5)
-#print_list()
-test_click()
+print_list()
 browser.quit()
 
