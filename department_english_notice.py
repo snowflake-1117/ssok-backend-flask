4b785f8a515f3091f429d9c5f6f26d228b035451
Herin Chung <lync2846@gmail.com>
2018-01-25 22:31:25 +0900

    find clue for iteration

--- department_english_notice.py
+++ department_english_notice.py
@@ -2,42 +2,54 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 import time
 
-
+# def read_loop():
+#     KEEP_CRAWLING = True
+#     while(KEEP_CRAWLING):
+#         print_list()
+#         KEEP_CRAWLING = move_to_next_page()
+#     return
 
 def print_list():
     titles = browser.find_elements_by_css_selector('td.list_td1')
     list_len = len(titles)//5
     print('count list: '+str(list_len))
-    START_OF_LIST_TD = 5
-    current_url = browser.current_url
-    for count in range(0, 2):
-        try:
-            a = browser.find_element_by_xpath('/html/body/form[2]/table/tbody/tr['+str(count+START_OF_LIST_TD)+']/td[3]/a')
-            href = a.get_attribute("href")
-            #print("href: ", href)
-            print(str(count+1)+"."+"제목: ",a.text)
-            print_link(a,current_url)
-        except NoSuchElementException:
-            print("-",a.text)
-
+    #START_OF_LIST_TD = 5
+    #current_url = browser.current_url
+    # for count in range(0, 2):
+    #     try:
+    #         a = browser.find_element_by_xpath('/html/body/form[2]/table/tbody/tr['+str(count+START_OF_LIST_TD)+']/td[3]/a')
+    #         #href = a.get_attribute("href")
+    #         #print("href: ", href)
+    #         print(str(count+1)+"."+"제목: ",a.text)
+    #         print_link(a,current_url)
+    #     except NoSuchElementException:
+    #         print("-",a.text)
     move_to_next_page()
     return
 
 def move_to_next_page():
     tr_list = browser.find_elements_by_xpath('/html/body/form[2]/table/tbody/tr[*]')
-    table_len = str(len(tr_list)-2)
-    page_list = browser.find_elements_by_xpath('/html/body/form[2]/table/tbody/tr['+table_len+']/td/table/tbody/tr/td[2]/*')
-    current_page = browser.find_element_by_xpath('/html/body/form[2]/table/tbody/tr['+table_len+']/td/table/tbody/tr/td[2]/b')
-    current_page_number = int(current_page.text[1:len(current_page.text)-1])
-    print('page:',current_page_number)
-    if(current_page_number==28):
-        print("END OF PAGE")
-    else:
-        current_item_count = page_list.index(current_page)
-        a = page_list.__getitem__(current_item_count+1)
-        a.click()
-        time.sleep(5)
+    page_location = str(len(tr_list)-2)
+    page_list = browser.find_elements_by_xpath('/html/body/form[2]/table/tbody/tr['+page_location+']/td/table/tbody/tr/td[2]/*')
+    current_page = browser.find_element_by_xpath('/html/body/form[2]/table/tbody/tr['+page_location+']/td/table/tbody/tr/td[2]/b')
+    CURRENT_NUMBER = int(current_page.text[1:len(current_page.text)-1])
+    page_last = page_list.__getitem__(len(page_list)-1)
+    LAST_NUMBER = page_last.text
+    print('page:',str(CURRENT_NUMBER)+'/'+LAST_NUMBER)
+    current_item_count = page_list.index(current_page)
+    a = page_list.__getitem__(current_item_count+1)
+    a.click()
+    time.sleep(5)
+    print_list()
+    # if(LAST_NUMBER!='[다음]'):
+    #     print("END OF PAGE")
+    # else:
+    #     current_item_count = page_list.index(current_page)
+    #     a = page_list.__getitem__(current_item_count+1)
+    #     a.click()
+    #     time.sleep(5)
     return
+
 
 def print_link(a,start_url):
     # a.click()
