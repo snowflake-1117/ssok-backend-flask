8bdf72d4b1b45691b076a5635ce4a6acd4b2e731
Herin Chung <lync2846@gmail.com>
2018-01-25 12:11:18 +0900

    extract only the tds of board

--- department_english_notice.py
+++ department_english_notice.py
@@ -1,34 +1,29 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
+import time
 
-def submit(href):
-    #browser.execute_script("return "+href)
-    #browser.find_element_by_xpath("//a[@href=\"javascript:page_link('" + str(count) + "')\"]").click()
-    browser.find_element_by_xpath("//a[@href=\""+href+"\"]").click()
-    return;
-
-def printList():
-    titles = browser.find_elements_by_xpath('/html/body/form[2]/table/tbody/tr[*]/td[3]')
+def print_list():
+    titles = browser.find_elements_by_css_selector('td.list_td1')
 
     for title in titles:
         try:
             a = title.find_element_by_css_selector("a")
-            global href
             href = a.get_attribute("href")
-            href = href[:len(href)-1]
             print("-", href)
-            print(title.text)
-            submit(href)
+            print("-",title.text)
+            # a.click()
         except NoSuchElementException:
-            print(title.text)
+            print("-",title.text)
+
     return;
 
 url = 'http://english.sookmyung.ac.kr/wiz/contents/board/board.php?home_id=english&handle=3'
 browser = webdriver.PhantomJS()
 browser.implicitly_wait(3)
 browser.get(url)
-
-printList()
+time.sleep(5)
+#print(browser.find_element_by_css_selector('html').text)
+print_list()
 
 browser.quit()
 
