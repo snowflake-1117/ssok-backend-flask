e446600ff9c517e09122071a37e6addb9c0ee9fd
Herin Chung <lync2846@gmail.com>
2018-01-27 08:32:55 +0900

    refactoring

--- department_english_notice.py
+++ department_english_notice.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 import time
 
 def print_list():
@@ -9,25 +8,22 @@ def print_list():
     START_OF_LIST_TD = 5
     count =0
     while count < list_len:
-        #list = browser.find_elements_by_xpath('/html/body/form[2]/table/tbody/tr[*]/td[3]/a')
-        a = browser.find_element_by_xpath('/html/body/form[2]/table/tbody/tr['+str(count+START_OF_LIST_TD)+']/td[3]/a')
-        print(str(count+1)+"."+"제목: ",a.text)
-        print_link(a)
+        item = browser.find_element_by_xpath('/html/body/form[2]/table/tbody/tr['+str(count+START_OF_LIST_TD)+']/td[3]/a')
+        print(str(count+1)+"."+"제목: ",item.text)
+        print_link(item)
         count+=1
-        #print("-",'NoSuchElementException')
     return
 
 def move_to_next_page():
     print_list()
     total_tr = browser.find_elements_by_xpath('/html/body/form[2]/table/tbody/tr[*]')
     page_location = str(len(total_tr) - 2)
-    page_list = browser.find_elements_by_xpath(
-        '/html/body/form[2]/table/tbody/tr[' + page_location + ']/td/table/tbody/tr/td[2]/*')
-    current_page = browser.find_element_by_xpath(
-        '/html/body/form[2]/table/tbody/tr[' + page_location + ']/td/table/tbody/tr/td[2]/b')
+    page_table = browser.find_element_by_xpath('/html/body/form[2]/table/tbody/tr[' + page_location + ']/td/table/tbody/tr/td[2]')
+    page_list = page_table.find_elements_by_css_selector('*')
+    current_page = page_table.find_element_by_css_selector('b')
     CURRENT_NUMBER = current_page.text
-    page_last = page_list.__getitem__(len(page_list) - 1)
-    LAST_NUMBER = page_last.text
+    last_page = page_list.__getitem__(len(page_list) - 1)
+    LAST_NUMBER = last_page.text
     print('page:', CURRENT_NUMBER + '/' + LAST_NUMBER)
     if(LAST_NUMBER !='[다음]' and CURRENT_NUMBER==LAST_NUMBER):
         print("END OF PAGE")
@@ -49,7 +45,7 @@ def print_link(a):
     time.sleep(5)
     return
 
-url = 'http://english.sookmyung.ac.kr/wiz/contents/board/board.php?home_id=english&handle=3'
+url = 'http://english.sookmyung.ac.kr/wiz/contents/board/board.php?home_id=english&handle=4'
 browser = webdriver.PhantomJS()
 browser.implicitly_wait(3)
 browser.get(url)
