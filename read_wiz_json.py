41938cbb0eeb6aa3d58591d1a15b5ffb11b7d610
Herin Chung <lync2846@gmail.com>
2018-01-28 10:47:22 +0900

    read json file

--- read_wiz_json.py
+++ read_wiz_json.py
@@ -3,8 +3,15 @@ from pprint import pprint
 
 data = json.load(open('wiz_departments.json'))
 
-pprint(data)
+# pprint(data)
 
 print('url개수 : ',len(data))
-#print(data[0]['college'])
+for count in range(0,len(data)):
+    print(str(count))
+    print('college:',data[count]['college'])
+    print('department:', data[count]['department'])
+    print('domain_name:', data[count]['domain_name'])
+    print('home_id:', data[count]['home_id'])
+    print('handle:', data[count]['handle'])
+#"college":"문과대학","department":"한국어문학부","domain_name":"korean","home_id":"korean","handle":1
 exit()
