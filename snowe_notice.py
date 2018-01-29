6ca47442f4003bc184c2e4931217f91021adeb7d
Herin Chung <lync2846@gmail.com>
2018-01-30 07:45:44 +0900

    change logic to extract data for db

--- snowe_notice.py
+++ snowe_notice.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
 from snowe_notice_board import *
 import time
 
@@ -24,17 +23,18 @@ def crawl_pages():
 
 def call_list():
     board = browser.find_element_by_xpath('//*[@id="messageListBody"]')
-    td_list = board.find_elements_by_css_selector('td.title')
-    for td in td_list:
-        try:
-            a = td.find_element_by_css_selector('a')
+    tr_list = board.find_elements_by_css_selector('tr')
+    for tr in tr_list:
+        if(tr.get_attribute('class')!='notice'):
+            title_head = tr.find_element_by_css_selector('td.title_head')
+            print("title_head.text:", title_head.text)
+            title = tr.find_element_by_css_selector('td.title')
+            a = title.find_element_by_css_selector('a')
             href = a.get_attribute('href')
             print("-", href)
             span = a.find_element_by_css_selector('span')
             print("-", span.text)
             print_page(href)
-        except NoSuchElementException:
-            print("-", td.text)
     return;
 
 global url
