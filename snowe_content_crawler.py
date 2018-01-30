2a5d1a92397c5bdc4ee861ace306a2c7a53cd1a7
Herin Chung <lync2846@gmail.com>
2018-01-30 09:38:31 +0900

    return content

--- snowe_content_crawler.py
+++ snowe_content_crawler.py
@@ -8,18 +8,18 @@ def print_page(url):
     article = soup.select_one('div.article')
     p_list = article.find_all('p')
     imgs = article.findAll('img')
+    content = ""
     if imgs:
         print('img:\n', "\n".join(set(tag['src'] for tag in imgs)))
     elif p_list:
-        content = ""
         for p in p_list:
             if p.text:
-                 content = content + p.text + '\n'
+                content = content + p.text + '\n'
         print('content:\n',content+'\n')
     else:
         board = article.select_one('div#_ckeditorContents')
         content = board.text
         print('content:\n',content+'\n')
 
-    return
+    return content
 
