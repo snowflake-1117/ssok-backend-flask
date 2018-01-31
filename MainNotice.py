e5991bc9f9434c13d1b9cee85596fe08d413ea60
Hyemin Kim <1117hyemin@gmail.com>
2018-01-31 15:06:46 +0900

    Apply save data to DB

--- MainNotice.py
+++ MainNotice.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup
 import time
 import re
 from NoticeData import NoticeData
+from UnivDBManager import UnivDBManager
 
 
 class MainNotice:
@@ -22,6 +23,7 @@ class MainNotice:
         last_notice_number = int(self.browser.find_element_by_class_name("num").text)
         last_notice_page = self.get_last_page(last_notice_number, 10)
         self.scrape_current_to_max_page(start_notice_page, last_notice_page)
+        self.save_notices_to_db()
 
     def quit(self):
         self.browser.quit()
@@ -34,7 +36,7 @@ class MainNotice:
 
     def scrape_current_to_max_page(self, start_page, last_page):
         current_page = start_page
-        while current_page <= last_page:
+        while current_page < last_page:
             print("page: " + str(current_page))
             page_notices = self.browser.find_elements_by_css_selector("a.artclLinkView")
             self.save_notices_data(page_notices)
@@ -62,6 +64,8 @@ class MainNotice:
         main_page_notice.large_category = notice_large_category.text
 
     def get_content_output(self, content_sentences):
+        if content_sentences is None:
+            return ""
         output = ""
         for content_sentence in content_sentences.contents:
             stripped = str(content_sentence).strip()
@@ -72,3 +76,8 @@ class MainNotice:
 
     def get_page_link(self, current_page):
         return "//a[@href=\"javascript:page_link('" + str(current_page) + "')\"]"
+
+    def save_notices_to_db(self):
+        for i in self.main_page_notice_list:
+            UnivDBManager.insert(1, i.large_category, i.large_category, i.title, i.content)
+            # To-do: change number
