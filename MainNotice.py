77ca10f54e2ca620245721f580d5630492b8b792
Hyemin Kim <1117hyemin@gmail.com>
2018-02-04 15:51:54 +0900

    Code refactoring

--- MainNotice.py
+++ MainNotice.py
@@ -17,7 +17,7 @@ class MainNotice:
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
         self.browser.get(self.notice_bbs_url)
-        self.main_page_notice_list = []
+        self.record_list = []
 
     def start(self):
         start_notice_page = 1
@@ -38,52 +38,34 @@ class MainNotice:
     def scrape_current_to_max_page(self, start_page, last_page):
         current_page = start_page
         while current_page <= last_page:
-            print("page: " + str(current_page))
             notice_list = self.browser.find_elements_by_css_selector("a.artclLinkView")
             notice_id_list = self.browser.find_elements_by_css_selector("td.num")
-            self.record_notices_data_to_list(notice_list,notice_id_list)
+            self.record_notices_data_to_list(notice_list, notice_id_list)
             self.browser.find_element_by_xpath(self.get_page_link(current_page)).click()
             current_page += 1
 
     def record_notices_data_to_list(self, notice_list, notice_id_list):
         for notice, notice_id in zip(notice_list, notice_id_list):
-            main_page_notice = Record()
-            notice_item_url = notice.get_attribute("href")
-            notice_item_response = urllib.request.urlopen(notice_item_url)
-            soup_notice = BeautifulSoup(notice_item_response, "html.parser")
-
-            self.record_id(notice_id, main_page_notice)
-            self.record_category_and_division(soup_notice, main_page_notice)
-            self.record_title(soup_notice, main_page_notice)
-            self.record_content(soup_notice, main_page_notice)
-            self.record_view(soup_notice, main_page_notice)
-            self.record_date(soup_notice, main_page_notice)
-            self.main_page_notice_list.append(main_page_notice)
+            soup_notice = self.get_soup(notice)
+            record = self.record_data(notice_id, soup_notice)
+            self.record_list.append(record)
             time.sleep(1)
 
-    def record_id(self, notice_id, main_page_notice):
-        main_page_notice.id = int(notice_id.text)
+    def get_soup(self, notice):
+        notice_item_url = notice.get_attribute("href")
+        notice_item_response = urllib.request.urlopen(notice_item_url)
+        return BeautifulSoup(notice_item_response, "html.parser")
 
-    def record_title(self, soup_notice, main_page_notice):
-        notice_title = soup_notice.select_one("div.view_top > dl > dt")
-        main_page_notice.title = self.get_content_output(notice_title)
-
-    def record_content(self, soup_notice, main_page_notice):
-        notice_content = soup_notice.select_one(".view_contents")
-        main_page_notice.content = self.get_content_output(notice_content)
-
-    def record_category_and_division(self, soup_notice, main_page_notice):
-        notice_category = soup_notice.select("div.view_top > dl > dd")[0]
-        main_page_notice.category = notice_category.text
-        main_page_notice.division = notice_category.text
-
-    def record_view(self, soup_notice, main_page_notice):
-        notice_view = soup_notice.select("div.view_top > dl > dd")[1]
-        main_page_notice.view = int(notice_view.text)
-
-    def record_date(self, soup_notice, main_page_notice):
-        notice_date = soup_notice.select("div.view_top > dl > dd")[2]
-        main_page_notice.date = datetime.strptime(notice_date.text, "%Y.%m.%d").date()
+    def record_data(self, notice_id, soup):
+        record = Record()
+        record.id = int(notice_id.text)
+        record.category = soup.select("div.view_top > dl > dd")[0].text
+        record.division = soup.select("div.view_top > dl > dd")[0].text
+        record.title = self.get_content_output(soup.select_one("div.view_top > dl > dt"))
+        record.content = self.get_content_output(soup.select_one(".view_contents"))
+        record.view = int(soup.select("div.view_top > dl > dd")[1].text)
+        record.date = datetime.strptime(soup.select("div.view_top > dl > dd")[2].text, "%Y.%m.%d").date()
+        return record
 
     def get_content_output(self, content_sentences):
         if content_sentences is None:
@@ -101,5 +83,5 @@ class MainNotice:
         return "//a[@href=\"javascript:page_link('" + str(current_page) + "')\"]"
 
     def save_notices_to_db(self):
-        for i in self.main_page_notice_list:
+        for i in self.record_list:
             DBManager.insert(i)
