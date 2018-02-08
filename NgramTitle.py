65b004d26302f15a64a69f513cef41e755aaa68a
Hyemin Kim <1117hyemin@gmail.com>
2018-02-08 17:17:16 +0900

    Apply printing result which are upper than 50% similarity

--- NgramTitle.py
+++ NgramTitle.py
@@ -1,3 +1,5 @@
+# -*- coding:utf-8 -*-]
+
 from NgramTitleData import NgramTitleData
 
 
@@ -6,29 +8,28 @@ class NgramTitle:
         self._sentence_list = sentence_list
         self._n_number = n_number
         self._ngram_data = []
+        self._indices_list = []
 
     def compare_sentence_list(self):
-        for object_index, object_sentence in self._sentence_list:
+        for object_index, object_sentence in enumerate(self._sentence_list):
             self.compare_sentence(object_index, object_sentence)
-        self.print_result()
 
     def compare_sentence(self, object_index, object_sentence):
         object_split_list = self.get_spilt_list_of(object_sentence)
-        for subject_index, subject_sentence in self._sentence_list:
-            if ([object_index, subject_index]) in self._ngram_data[:][0].two_indices_set:
-                break
-            else:
+        for subject_index, subject_sentence in enumerate(self._sentence_list):
+            if subject_index > object_index:
                 subject_split_list = self.get_spilt_list_of(subject_sentence)
                 count = 0
                 for i in object_split_list:
                     for j in subject_split_list:
                         if i == j:
                             count += 1
-            ngram_data = NgramTitleData()
-            ngram_data.set_of_two_titles = ([object_sentence, subject_sentence])
-            ngram_data.set_of_two_indices = ([object_index, subject_index])
-            ngram_data.similarity_between_two_sentences = count / len(object_split_list)
-            self._ngram_data.append(ngram_data)
+                ngram_data = NgramTitleData()
+                ngram_data.list_of_two_titles = [object_sentence, subject_sentence]
+                ngram_data.list_of_two_indices = [object_index, subject_index]
+                ngram_data.similarity_between_two_sentences = count / len(object_split_list)
+                self._ngram_data.append(ngram_data)
+                self._indices_list.append([object_index, subject_index])
 
     def get_spilt_list_of(self, sentence):
         split_list = []
@@ -38,6 +39,9 @@ class NgramTitle:
             split_list.append(split_part)
         return split_list
 
-    def print_result(self):
+    def print_result_upper_50_percent_similarity(self):
         for i in self._ngram_data:
-            print(i.print_all)
+            if i.similarity_between_two_sentences >= 0.5:
+                print("index list: ", i.list_of_two_indices,
+                      "\ntitle list: ", i.list_of_two_titles,
+                      "\nsimilarity: ", i.similarity_between_two_sentences)
