36b6d19d5ddf729c43515a2b3d5939f795b27828
Hyemin Kim <1117hyemin@gmail.com>
2018-02-08 17:15:09 +0900

    Modify name and type of methods

--- NgramTitleData.py
+++ NgramTitleData.py
@@ -1,34 +1,29 @@
 class NgramTitleData:
     def __init__(self):
-        self._set_of_two_titles = set([])
-        self._set_of_two_indices = set([])
+        self._list_of_two_titles = []
+        self._list_of_two_indices = []
         self._similarity_between_two_sentences = 0.0
 
     @property
-    def set_of_two_indices(self):
-        return self._set_of_two_indices
+    def list_of_two_indices(self):
+        return self._list_of_two_indices
 
     @property
     def similarity_between_two_sentences(self):
         return self._similarity_between_two_sentences
 
     @property
-    def set_of_two_titles(self):
-        return self._set_of_two_titles
+    def list_of_two_titles(self):
+        return self._list_of_two_titles
 
-    @set_of_two_indices.setter
-    def set_of_two_indices(self, indices_set):
-        self._set_of_two_indices = indices_set
+    @list_of_two_indices.setter
+    def list_of_two_indices(self, indices_list):
+        self._list_of_two_indices = indices_list
 
     @similarity_between_two_sentences.setter
     def similarity_between_two_sentences(self, similarity):
         self._similarity_between_two_sentences = similarity
 
-    @set_of_two_titles.setter
-    def set_of_two_titles(self, title_set):
-        self._set_of_two_titles = title_set
-
-    def print_all(self):
-        print("index set: ", self._set_of_two_indices,
-              "title set: ", self._set_of_two_titles,
-              "similarity: ",self._similarity_between_two_sentences,)
+    @list_of_two_titles.setter
+    def list_of_two_titles(self, title_list):
+        self._list_of_two_titles = title_list
