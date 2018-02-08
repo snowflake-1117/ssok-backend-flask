1e0ca3e89febe7ce8fdb5663d059057782ab62be
Hyemin Kim <1117hyemin@gmail.com>
2018-02-08 16:15:26 +0900

    Save also titles and Apply print method

--- NgramTitle.py
+++ NgramTitle.py
@@ -7,17 +7,10 @@ class NgramTitle:
         self._n_number = n_number
         self._ngram_data = []
 
-    def get_spilt_list_of(self, sentence):
-        split_list = []
-        sentence_length = len(sentence) - self._n_number + 1
-        for i in range(sentence_length):
-            split_part = sentence[i:i + self._n_number]
-            split_list.append(split_part)
-        return split_list
-
     def compare_sentence_list(self):
         for object_index, object_sentence in self._sentence_list:
             self.compare_sentence(object_index, object_sentence)
+        self.print_result()
 
     def compare_sentence(self, object_index, object_sentence):
         object_split_list = self.get_spilt_list_of(object_sentence)
@@ -32,6 +25,19 @@ class NgramTitle:
                         if i == j:
                             count += 1
             ngram_data = NgramTitleData()
-            ngram_data.two_indices_set = ([object_index, subject_index])
+            ngram_data.set_of_two_titles = ([object_sentence, subject_sentence])
+            ngram_data.set_of_two_indices = ([object_index, subject_index])
             ngram_data.similarity_between_two_sentences = count / len(object_split_list)
             self._ngram_data.append(ngram_data)
+
+    def get_spilt_list_of(self, sentence):
+        split_list = []
+        sentence_length = len(sentence) - self._n_number + 1
+        for i in range(sentence_length):
+            split_part = sentence[i:i + self._n_number]
+            split_list.append(split_part)
+        return split_list
+
+    def print_result(self):
+        for i in self._ngram_data:
+            print(i.print_all)
--- NgramTitleData.py
+++ NgramTitleData.py
@@ -1,20 +1,34 @@
 class NgramTitleData:
     def __init__(self):
-        self._two_indices_set = set([])
+        self._set_of_two_titles = set([])
+        self._set_of_two_indices = set([])
         self._similarity_between_two_sentences = 0.0
 
     @property
-    def two_indices_set(self):
-        return self._two_indices_set
+    def set_of_two_indices(self):
+        return self._set_of_two_indices
 
     @property
     def similarity_between_two_sentences(self):
         return self._similarity_between_two_sentences
 
-    @two_indices_set.setter
-    def two_indices_set(self, indices_set):
-        self._two_indices_set = indices_set
+    @property
+    def set_of_two_titles(self):
+        return self._set_of_two_titles
+
+    @set_of_two_indices.setter
+    def set_of_two_indices(self, indices_set):
+        self._set_of_two_indices = indices_set
 
     @similarity_between_two_sentences.setter
     def similarity_between_two_sentences(self, similarity):
         self._similarity_between_two_sentences = similarity
+
+    @set_of_two_titles.setter
+    def set_of_two_titles(self, title_set):
+        self._set_of_two_titles = title_set
+
+    def print_all(self):
+        print("index set: ", self._set_of_two_indices,
+              "title set: ", self._set_of_two_titles,
+              "similarity: ",self._similarity_between_two_sentences,)
