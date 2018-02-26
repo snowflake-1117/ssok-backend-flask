f63f0e2b640a3f11ec2d343d655dcacc7fc262db
lyn <lync2846@gmail.com>
2018-02-27 00:46:32 +0900

    update clssified result

---
+++ Bayesian_Classifier.py
@@ -0,0 +1,17 @@
+from NaiveDBManager_department import DBManager
+from BayesianFilter import BayesianFilter
+import csv
+
+bf = BayesianFilter()
+
+DBManager()
+train_set = DBManager.selectTrainData()
+for row in train_set:
+    bf.fit(row[0], row[1])
+
+test_set = DBManager.selectTestData()
+equals = 0
+
+for row in test_set:
+    pre, scorelist = bf.predict(row[0])
+    DBManager.updateAt(row[0], pre)
--- NaiveDBManager_department.py
+++ NaiveDBManager_department.py
@@ -2,8 +2,6 @@ from pymysql.err import InternalError
 import pymysql.cursors
 
 class DBManager:
-    YOUR_USER = 'admin'
-    YOUR_PW = '2846'
     USER = YOUR_USER
     PW = YOUR_PW
 
@@ -93,3 +91,20 @@ class DBManager:
             for row in result:
                 rows.append(row)
         return rows
+
+
+    @staticmethod
+    def updateAt(title, division):
+        conn = pymysql.connect(host='localhost',
+                               user=DBManager.USER,
+                               password=DBManager.PW,
+                               db='sookmyung',
+                               charset='utf8mb4')
+
+        with conn.cursor() as cursor:
+            sql = " update univ "
+            sql = sql + "set division='"+division+"' "
+            sql = sql + "where title='"+title+"';"
+            cursor.execute(sql)
+            conn.commit()
+        return
