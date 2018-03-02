5fbf3fadcdffb4be567106dac9b42dcaf0aa0b7d
lyn <lync2846@gmail.com>
2018-03-02 14:56:23 +0900

    hotfix for get test division list

--- NaiveDBManager_department.py
+++ NaiveDBManager_department.py
@@ -69,7 +69,7 @@ class DBManager:
                 sql = "select title, division, category from univ where "
                 for category in categoryList:
                     if DBManager.isDepartment(category):
-                        sql = sql + "category='" +  category[0] + "' or "
+                        sql = sql + "category='" +  category + "' or "
 
                 sql = sql[:len(sql) - 4]
                 print("sql_test:",sql)
@@ -84,29 +84,14 @@ class DBManager:
 
     @staticmethod
     def getTestCategoryList():
-        rows = []
-        conn = pymysql.connect(host='localhost',
-                               user=DBManager.USER,
-                               password=DBManager.PW,
-                               db='sookmyung',
-                               charset='utf8mb4')
-
-        with conn.cursor() as cursor:
-            sql = " select distinct(category) from univ "
-            sql = sql + "group by category "
-            sql = sql + "having count(distinct(division))=1;"
-            cursor.execute(sql)
-            conn.commit()
-            result = cursor.fetchall()
-            for row in result:
-                rows.append(row)
+        rows = ["역사문화학과", "독일언어문화학과", "프랑스언어문화학과", "문헌정보학과", "체육교육과", "응용물리전공", "소프트웨어학부",
+                "식품영양학과", "정치외교학과", "행정학과", "환경디자인학과", "글로벌서비스학부", "테슬", "화학과", "생명시스템학부", "회화과"
+                , "무용과", "화공생명학부", "전자공학전공", "기계시스템학부", "가족자원경영학과", "소비자경제학과", "미디어학부", "약햑대학"]
         return rows
 
 
     @staticmethod
     def updateAt(title, division):
-        title.replace("\'", "\'\'")
-        title.replace("\"", "\"\"")
         conn = pymysql.connect(host='localhost',
                                user=DBManager.USER,
                                password=DBManager.PW,
@@ -114,9 +99,7 @@ class DBManager:
                                charset='utf8mb4')
 
         with conn.cursor() as cursor:
-            sql = " update univ "
-            sql = sql + "set division='"+division+"' "
-            sql = sql + "where title='"+title+"';"
-            cursor.execute(sql)
+            sql = "update univ set division=%s where title=%s;"
+            cursor.execute(sql, (division, title))
             conn.commit()
         return
--- README.md
+++ README.md
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-# scraping-sookpam
-=======
 sookpam-backend-flask
->>>>>>> fb590476e4026705925084910963bd4a3844f3c4
