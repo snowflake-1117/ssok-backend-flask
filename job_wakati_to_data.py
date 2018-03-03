08d6a22da74e0ac51a219e31dbf2373d0c9a48b6
lyn <lync2846@gmail.com>
2018-03-03 23:11:32 +0900

    change to apply for entire data

--- job_wakati_to_data.py
+++ job_wakati_to_data.py
@@ -96,7 +96,7 @@ def count_freq_test(limit = 0):
 
 
 if not os.path.exists(train_data):
-    X, Y = count_freq_train(100)  # 전체는 void
+    X, Y = count_freq_train()  # 전체는 void
     json.dump({"X": X, "Y": Y}, open(train_data, "w"))
-    X, Y = count_freq_test(100)  # 전체는 void
+    X, Y = count_freq_test()  # 전체는 void
     json.dump({"X": X, "Y": Y}, open(test_data, "w"))
