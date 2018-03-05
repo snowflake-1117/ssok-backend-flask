f0f300e9b6071c6f52586105ec9debdf6c6f2f6a
Hyemin Kim <1117hyemin@gmail.com>
2018-03-05 16:01:59 +0900

    Modify filter condition

--- RecommendHelper.py
+++ RecommendHelper.py
@@ -51,7 +51,7 @@ class RecommendHelper:
     @classmethod
     def filter_by_uninteresting_division(cls, uninteresting_division_list, sql):
         if uninteresting_division_list.__len__() > 0:
-            sql += ' AND not division in ('
+            sql += ' AND (not division in ('
             for index, uninteresting_division in enumerate(uninteresting_division_list):
                 sql += "\"" + uninteresting_division + "\""
                 if index < uninteresting_division_list.__len__() - 1:
@@ -80,14 +80,7 @@ class RecommendHelper:
             if index < major_list.__len__() - 1:
                 sql += ", "
             else:
-                sql += ')'
-        return sql
-
-    @classmethod
-    def add_student_info_condition(cls, recommend_condition, sql):
-        sql = cls.filter_by_student_status(recommend_condition, sql)
-        sql = cls.filter_by_student_grade(recommend_condition, sql)
-        sql += ' OR (content LIKE \"%' + recommend_condition.student_year + '학번%\")'
+                sql += '))'
         return sql
 
     @classmethod
@@ -101,13 +94,20 @@ class RecommendHelper:
     @classmethod
     def filter_by_student_grade(cls, recommend_condition, sql):
         if recommend_condition.student_grade is 1:
-            sql += ' OR (title LIKE \"%신입생%\" OR title LIKE \"%새내기%\" OR title LIKE \"%GELT%\" OR content LIKE \"% 1학년%\"))'
+            sql += ' OR (title LIKE \"%신입생%\" OR title LIKE \"%새내기%\" OR title LIKE \"%GELT%\" OR content LIKE \"% 1학년%\")'
         elif recommend_condition.student_grade is 2:
-            sql += ' OR (title LIKE \"% 2학년%\" OR title LIKE \"%전과%\"  OR content LIKE \"%3학기%\" OR content LIKE \"%4학기%\" OR title LIKE \"%복수전공%\" OR title LIKE \"%부전공%\" OR title LIKE \"%교환학생%\"))'
+            sql += ' OR (title LIKE \"% 2학년%\" OR title LIKE \"%전과%\"  OR content LIKE \"%3학기%\" OR content LIKE \"%4학기%\" OR title LIKE \"%복수전공%\" OR title LIKE \"%부전공%\" OR title LIKE \"%교환학생%\")'
         elif recommend_condition.student_grade is 3:
             sql += ' OR (title LIKE \"%3학년%\" OR title LIKE \"%조기졸업%\" OR content LIKE \"%5학기%\" OR content LIKE \"%6학기%\" OR title LIKE \"%교환학생%\")'
         else:
-            sql += ' OR (title LIKE \"%수료%\" OR title LIKE \"%졸업%\" OR title LIKE \"%학위수여%\"))'
+            sql += ' OR (title LIKE \"%수료%\" OR title LIKE \"%졸업%\" OR title LIKE \"%학위수여%\")'
+        return sql
+
+    @classmethod
+    def add_student_info_condition(cls, recommend_condition, sql):
+        sql = cls.filter_by_student_status(recommend_condition, sql)
+        sql = cls.filter_by_student_grade(recommend_condition, sql)
+        sql += ' OR (content LIKE \"%' + str(recommend_condition.student_year) + '학번%\"))'
         return sql
 
     @classmethod
@@ -116,11 +116,6 @@ class RecommendHelper:
             return filtered_recommend_list
         else:
             selected_recommend_list = []
-            if recommend_condition.INTEREST_SCHOLARSHIP is not 2:
-                # recommend_condition.school_scholar
-                # recommend_condition.government_scholar
-                # recommend_condition.external_scholar
-                pass
-            # recommend_condition.student_grade
-            # recommend_condition.student_year
+            for recommend_item in filtered_recommend_list:
+                selected_recommend_list.append()
             return selected_recommend_list
