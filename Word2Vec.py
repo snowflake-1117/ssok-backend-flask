421f094f49b1dc7de7af2599cd89984ce517e7d4
LAPTOP-4ETK11SN\jiun <juneoh227@gmail.com>
2018-03-11 18:01:46 +0900

    changed to class method

--- Word2Vec.py
+++ Word2Vec.py
@@ -3,6 +3,7 @@ from gensim.models import word2vec
 class W2V:
     model = word2vec.Word2Vec.load("whole.model")
 
+    @classmethod
     def get_similar_words(cls, target_word, number):
         results = cls.model.most_similar(positive=[target_word])[0:number]
         return results
