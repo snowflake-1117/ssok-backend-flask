b945e089b353775478c1eaebd9f9d2b8cd27e320
Hyemin Kim <1117hyemin@gmail.com>
2018-03-16 22:55:37 +0900

    Change method to static

--- RelevantContentCommender.py
+++ RelevantContentCommender.py
@@ -6,48 +6,50 @@ class RecommendDatum:
         self.subject_line = sentence
         self.similarity = similarity
 
+    @staticmethod
+    def compare_with(sentence, sentence_list):
+        data_list = []
+        object_sentence = sentence
+        object_split_list = RecommendDatum.get_spilt_list_of(object_sentence)
 
-def compare_with(sentence, sentence_list):
-    data_list = []
-    object_sentence = sentence
-    object_split_list = get_spilt_list_of(object_sentence)
+        for subject_index, subject_sentence in enumerate(sentence_list):
+            if subject_sentence != object_sentence:
+                subject_split_list = RecommendDatum.get_spilt_list_of(subject_sentence)
+                count = 0
+                for i in object_split_list:
+                    for j in subject_split_list:
+                        if i == j:
+                            count += 1
+                datum = RecommendDatum(subject_sentence, count / len(object_split_list))
+                data_list.append(datum)
 
-    for subject_index, subject_sentence in enumerate(sentence_list):
-        if subject_sentence != object_sentence:
-            subject_split_list = get_spilt_list_of(subject_sentence)
-            count = 0
-            for i in object_split_list:
-                for j in subject_split_list:
-                    if i == j:
-                        count += 1
-            datum = RecommendDatum(subject_sentence, count / len(object_split_list))
-            data_list.append(datum)
+        sorted_list = sorted(data_list, key=lambda item: item.similarity, reverse=True)
+        response = RecommendDatum.filter(sorted_list)
+        response_list = DBManager.select_max_list(response)
+        print("response>")
+        for record in response_list[:2]:
+            print(record)
+        return response_list
 
-    sorted_list = sorted(data_list, key=lambda item: item.similarity, reverse=True)
-    response = filter(sorted_list)
-    response_list = DBManager.select_max_list(response)
-    print("response>")
-    for record in response_list[:2]:
-        print(record)
-    return response_list
+    @staticmethod
+    def get_spilt_list_of(sentence):
+        split_list = []
+        sentence_length = len(sentence) - 2 + 1
+        for i in range(sentence_length):
+            split_part = sentence[i:i + 2]
+            split_list.append(split_part)
+        return split_list
 
-
-def get_spilt_list_of(sentence):
-    split_list = []
-    sentence_length = len(sentence) - 2 + 1
-    for i in range(sentence_length):
-        split_part = sentence[i:i + 2]
-        split_list.append(split_part)
-    return split_list
-
-
-def filter(sorted_list):
-    max_data = []
-    dup = False
-    for datum in sorted_list:
-        try:
-            m = max_data.index(datum)
-        except ValueError:
-            if max_data.__len__() < 2:
-                max_data.append(datum)
-    return max_data
+    @staticmethod
+    def filter(sorted_list):
+        n_number = 2
+        max_data = []
+        for datum in sorted_list:
+            try:
+                m = max_data.index(datum)
+            except ValueError:
+                if max_data.__len__() < n_number:
+                    max_data.append(datum)
+            if max_data.__len__() > n_number:
+                break
+        return max_data
