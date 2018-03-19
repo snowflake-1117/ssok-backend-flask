52814bbb89a6d43714b50a5aab63264ba82ec652
오지은 <juneoh227@gmail.com>
2018-03-19 22:20:11 +0900

    Update Word2Vec.py
    deleted var number

--- Word2Vec.py
+++ Word2Vec.py
@@ -4,9 +4,9 @@ class W2V:
     model = word2vec.Word2Vec.load("whole.model")
 
     @classmethod
-    def get_similar_words(cls, target_word, number):
+    def get_similar_words(cls, target_word):
         try:
-            results = cls.model.most_similar(positive=[target_word])[0:number]
+            results = cls.model.most_similar(positive=[target_word])[0:4]
             return results
         except Exception as e:
             return None
